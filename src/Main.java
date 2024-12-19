306f2b99c3b6e6cdc842ee32904d34dcd43e482f
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:29:54 +0200

    Update Main.java
    Remove comments

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,19 @@ import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String pathToFileWeAreCreating = "C:\\Users\\Admin\\Desktop\\Example replace.txt"; //создаем строковую переменную, в
-        //которую пишем адрес и имя файла, который хотим создать и записать туда данные
-        replaceSystemOut(pathToFileWeAreCreating); //вызываем метод для подменны команды вывода в консоль System.out - на
-        //команду записи в файл System.out
+        String pathToFileWeAreCreating = "C:\\Users\\Admin\\Desktop\\Example replace.txt";
+        replaceSystemOut(pathToFileWeAreCreating);
     }
 
-    public static void replaceSystemOut(String pathToFile) throws FileNotFoundException { //метод для подменны команды вывода
-        // в консоль System.out - на команду записи в файл System.out
-        PrintStream printStream = new PrintStream(new File(pathToFile)); //создаем обьект класса PrintStream, который в свою
-        // очередь отвечает за вывод данных и указываем в нем путь - место куда мы будем выводить данные
+    public static void replaceSystemOut(String pathToFile) throws FileNotFoundException { 
+        PrintStream printStream = new PrintStream(new File(pathToFile)); 
+        
+        PrintStream ourIDEAConsole = System.out;
 
-        PrintStream ourIDEAConsole = System.out; //в переменную типа PrintStream сохраним оригинал для отображения данных (т.е.
-        //для отображения в консоли)
+        System.setOut(printStream);
+        System.out.println("This line writing to the file...");
 
-        System.setOut(printStream); //с помощью setOut, устанавливаем новый адрес вывода данных
-        System.out.println("This line writing to the file..."); //выполняем процесс вывода (как обычно мы это делаем для
-        //вывода в консоль)
-
-        System.setOut(ourIDEAConsole); //с помощью setOut, возвращаем базовый адрес вывода данных (в консоль IDEA)
-        System.out.println("This line writing to the console..."); //пробуем выполнить
+        System.setOut(ourIDEAConsole);
+        System.out.println("This line writing to the console...");
     }
 }
